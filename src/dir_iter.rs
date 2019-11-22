8926619880d368e6187b6265b56a6407d7441622
aQaTL <mmsoltys@outlook.com>
2019-11-22 13:46:14 +0100

    MVP

---
+++ src/dir_iter.rs
@@ -0,0 +1,52 @@
+use std::{
+	path::Path,
+	fs::DirEntry,
+	io,
+};
+
+pub struct DirIter {
+	stack: Vec<DirEntry>,
+}
+
+impl DirIter {
+	pub fn new(path: &Path) -> io::Result<Self> {
+		let mut dir_iter = DirIter {
+			stack: Vec::new(),
+		};
+		dir_iter.read_dir(path)?;
+		Ok(dir_iter)
+	}
+
+	fn read_dir(&mut self, dir: &Path) -> io::Result<()> {
+		let mut entries = dir
+			.read_dir()?
+			.filter_map(Result::ok)
+			.collect::<Vec<_>>();
+		self.stack.append(&mut entries);
+		Ok(())
+	}
+}
+
+impl Iterator for DirIter {
+	type Item = io::Result<DirEntry>;
+
+	fn next(&mut self) -> Option<io::Result<DirEntry>> {
+		let last = match self.stack.pop() {
+			Some(last) => last,
+			None => return None,
+		};
+
+		let metadata = match last.metadata() {
+			Ok(m) => m,
+			Err(e) => return Some(Err(e)),
+		};
+		
+		if metadata.is_dir() {
+			if let Err(e) = self.read_dir(&last.path()) {
+				return Some(Err(e));
+			}
+		}
+		
+		Some(Ok(last))
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,82 @@
+mod dir_iter;
+
+fn clap_app() -> clap::App<'static, 'static> {
+	use clap::*;
+	App::new(env!("CARGO_PKG_NAME"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.about(env!("CARGO_PKG_DESCRIPTION"))
+		.arg(Arg::with_name("FILE")
+			.required(false)
+			.index(1)
+			.takes_value(true))
+}
+
+fn main() {
+	let matches = clap_app().get_matches();
+	let file = matches
+		.value_of("FILE")
+		.map_or_else(
+			|| std::env::current_dir().expect("Failed to get current dir"),
+			|file| std::path::PathBuf::from(file));
+
+	println!("Searched dir: {}", file.display());
+
+	let mut size_sum: u64= 0;
+
+	for file in dir_iter::DirIter::new(&file).expect("Failed to read dir").filter_map(Result::ok) {
+		let metadata = match file.metadata() {
+			Ok(m) => m,
+			Err(e) => {
+				eprintln!("Failed to get metadata for {:?} with error: {}", file.path(), e);
+				continue;
+			},
+		};
+
+		if metadata.is_dir() {
+			continue;
+		}
+
+		size_sum += metadata.len();
+	}
+
+	println!("{} B", size_sum);
+	if size_sum > 1024 {
+		let hr = bytes_in_human_readable(size_sum);
+		println!("{:.1} {}B\n{:.1} {}B", hr.bin_value, hr.bin_name, hr.si_value, hr.si_name);
+	}
+}
+
+struct Multiple<'a> {
+	si_name: &'a str, 
+	si_value: f32, 
+
+	bin_name: &'a str, 
+	bin_value: f32, 
+}
+
+fn bytes_in_human_readable(size: u64) -> Multiple<'static> {
+	let mut bits = 0u64;
+
+	static MULTIPLES: [(&'static str, &'static str); 8] = [
+		("K", "Ki"),
+		("M", "Mi"),
+		("G", "Gi"),
+		("T", "Ti"),
+		("P", "Pi"),
+		("E", "Ei"),
+		("Z", "Zi"),
+		("Y", "Yi"),
+	];
+
+	while size >> (bits + 10) != 0 {
+		bits += 10;
+	}
+
+	Multiple {
+		si_name: MULTIPLES[(bits / 10 - 1) as usize].0,
+	 	si_value: (size as f32 / 10_f32.powi((3 * bits / 10) as i32)),
+
+		bin_name: MULTIPLES[(bits / 10 - 1) as usize].1,
+	 	bin_value: (size as f32 / 2_f32.powi(bits as i32)),
+	}
+}
